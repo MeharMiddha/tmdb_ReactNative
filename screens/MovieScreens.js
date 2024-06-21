54f4e16a31f167706decba0b0950c15246ee0af9
Mehar Middha <meharmiddha@Mehars-MacBook-Pro.local>
2024-06-21 12:06:18 +0530

    i have shown the person name image and character namer like cast of the film

--- screens/MovieScreens.js
+++ screens/MovieScreens.js
@@ -1,28 +1,103 @@
-import { Dimensions, Platform, ScrollView, TouchableOpacity, View } from 'react-native'
-import React, { useEffect } from 'react'
-import { useRoute } from '@react-navigation/native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { ChevronLeftIcon } from 'react-native-heroicons/outline';
-import { styles } from '../theme/theme';
+import {
+  Dimensions,
+  Image,
+  Platform,
+  ScrollView,
+  TouchableOpacity,
+  Text,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { useRoute } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ChevronLeftIcon } from "react-native-heroicons/outline";
+import { HeartIcon } from "react-native-heroicons/solid";
+import { styles, theme } from "../theme/theme";
+import { useNavigation } from "@react-navigation/native";
+import { LinearGradient } from "expo-linear-gradient";
+import Cast from '../components/Cast';
 
-var {width, height} = Dimensions.get('window');
-const ios = Platform.OS == 'ios';
-const topMargin = ios ? '':'mt-3';
+var { width, height } = Dimensions.get("window");
+const ios = Platform.OS == "ios";
+const topMargin = ios ? "" : "mt-3";
 
 export default function MovieScreens() {
-    const {params:item} = useRoute();
-    useEffect(()=>{
-        // call the mopvie details api
-    },[item])
+  let movieName = "Ant-Man and the Wasp";
+  const navigation = useNavigation();
+  const [isFavourite, setIsFavourite] = useState(false);
+  const [cast, setCast] = useState([1,2,3,4,5]);
+  const { params: item } = useRoute();
+  useEffect(() => {
+    // call the mopvie details api
+  }, [item]);
   return (
-    <ScrollView contentContainerStyle={{paddingBottom: 20}} className='flex-1 bg-neutral-900'>
-        <View className='w-full'>
-            <SafeAreaView className={'absolute z-20 w-full flex-row justify-between items-center px-4'+topMargin}>
-                <TouchableOpacity style={styles.background} className='rounded-xl p-1'>
-                    <ChevronLeftIcon size={28} strokeWidth={2.5} color="white" />
-                </TouchableOpacity>
-            </SafeAreaView>
+    <ScrollView
+      contentContainerStyle={{ paddingBottom: 20 }}
+      className="flex-1 bg-neutral-900"
+    >
+      <View className="w-full">
+        <SafeAreaView
+          className={
+            "absolute z-20 w-full flex-row justify-between items-center px-4" +
+            topMargin
+          }
+        >
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={styles.background}
+            className="rounded-xl p-1"
+          >
+            <ChevronLeftIcon size={28} strokeWidth={2.5} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setIsFavourite(!isFavourite)}>
+            <HeartIcon
+              size="35"
+              color={isFavourite ? theme.background : "white"}
+            />
+          </TouchableOpacity>
+        </SafeAreaView>
+        <View>
+          <Image
+            source={{
+              uri: "https://i.pinimg.com/originals/30/78/27/307827bc6024420e5fdf229318261587.jpg",
+            }}
+            style={{ width, height: height * 0.55 }}
+          />
+          <LinearGradient
+            colors={["transparent", "rgba(23,23,23,0.8)", "rgba(23,23,23,1)"]}
+            style={{ width, height: height * 0.4 }}
+            start={{ x: 0.5, y: 0 }}
+            end={{ x: 0.5, y: 1 }}
+            className="absolute bottom-0"
+          />
         </View>
+      </View>
+      <View style={{ marginTop: -(height * 0.09) }} className="space-y-3">
+        <Text className="text-white text-center text-3xl font-bold tracking-wider">
+          {movieName}
+        </Text>
+        <Text className="text-neutral-400 font-semibold text-base text-center">
+          Released . 2020 . 180 min
+        </Text>
+        <View className="flex-row justify-center mx-4 space-x-2">
+          <Text className="text-neutral-400 font-semibold text-base text-center">
+            Action .
+          </Text>
+          <Text className="text-neutral-400 font-semibold text-base text-center">
+            Thrill .
+          </Text>
+          <Text className="text-neutral-400 font-semibold text-base text-center">
+            Comedy .
+          </Text>
+        </View>
+        <Text className="text-neutral-400 mx-4 tracking-wide">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Exercitationem ad eveniet magnam laudantium iusto distinctio sequi
+          quis totam eligendi quos. Sit odit doloribus, praesentium dolores enim
+          earum cumque numquam aspernatur.
+        </Text>
+      </View>
+      <Cast cast={cast} />
     </ScrollView>
-  )
+  );
 }
