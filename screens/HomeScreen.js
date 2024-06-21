ef279de75148a65339bb7e33cdd557fb98170d36
Mehar Middha <meharmiddha@Mehars-MacBook-Pro.local>
2024-06-21 14:11:09 +0530

    made the componennt of Search bar and applied navigation in home screen and also do the navigation router in app navigation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,12 +6,14 @@ import { Bars3CenterLeftIcon, MagnifyingGlassIcon } from 'react-native-heroicons
 import { styles } from '../theme/theme';
 import TrendingMovies from '@/components/TrendingMovies';
 import MovieList from '@/components/MovieList';
+import { useNavigation } from '@react-navigation/native';
 
 const ios = Platform.OS == 'ios';
 export default function HomeScreen() {
   const [trending,setTrending] = useState([1,2,3]);
   const [upcoming,setUpcoming] = useState([1,2,3]);
   const [topRated,setTopRated] = useState([1,2,3]);
+  const navigation = useNavigation();
   return ( 
     <View className="flex-1 bg-neutral-800">
         <SafeAreaView className={ios ? "-mb-2" : "mb-3"}>
@@ -19,7 +21,7 @@ export default function HomeScreen() {
           <View className='flex-row justify-between items-center mx-4'>
             <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
             <Text className='text-white text-3xl font-bold'><Text style={styles.text}>T</Text>MDB</Text>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={()=> navigation.navigate('Search')}>
               <MagnifyingGlassIcon size="30" strokeWidth={2} color="white" />
             </TouchableOpacity>
           </View>
